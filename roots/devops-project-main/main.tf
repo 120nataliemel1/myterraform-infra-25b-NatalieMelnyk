04e753863b1e79e146d42f3f009bae43b3285dec
Altoha <altair.yed@edu.312school.com>
2026-01-30 22:47:07 -0500

    Added module for IAM-ROle creation

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -12,3 +12,15 @@ module "module2" {
   input_from_module1 = module.module1.greeting_message
   # ... any other required variables for module2
 }
+
+module "iam_roles" {
+  source   = "../child/iam-roles"
+  for_each = var.iam_roles
+
+  name     = each.key        
+  principal_type = each.value.principal_type
+  principal      = each.value.principal
+  service  = each.value.service
+  action   = each.value.action
+  resource = each.value.resource
+}
