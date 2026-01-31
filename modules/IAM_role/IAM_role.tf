3d3090dc8e392321072ead067f0f8c77ddee12c1
Altoha <altair.yed@edu.312school.com>
2026-01-30 22:45:15 -0500

    Added policy and policy attaachment

--- modules/IAM_role/IAM_role.tf
+++ modules/IAM_role/IAM_role.tf
@@ -20,3 +20,25 @@ resource "aws_iam_role" "iam_role" {
     tag-key = var.name
   }
 }
+
+resource "aws_iam_policy" "iam_policy" {
+  name = "${var.name}-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = var.action
+      
+        Resource = var.resource
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.iam_role.name
+  policy_arn = aws_iam_policy.iam_policy.arn
+}
