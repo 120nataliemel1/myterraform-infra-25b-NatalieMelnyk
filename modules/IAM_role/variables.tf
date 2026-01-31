a5425fd5f585c28f1127c920d5fd0844f94c664b
Altoha <altair.yed@edu.312school.com>
2026-01-30 22:46:09 -0500

    Declared variables

---
+++ modules/IAM_role/variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  type        = string
+  description = "Name of the IAM role"
+}
+
+
+variable "action" {
+  type = list(string)
+  description = "IAM policy action"
+}
+
+variable "resource" {
+  type        = list(string)
+  description = "Resources the policy applies to"
+}
+
+variable "principal_type" {
+  type    = string
+  description = "Either 'Service' or 'AWS'"
+  default = "Service"
+}
+
+variable "principal" {
+  type        = string
+  description = "ARN of IAM user/role or service name"
+}
