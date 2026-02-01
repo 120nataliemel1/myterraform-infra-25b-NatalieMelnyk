6f8ef21ce5937ff206343d526228175cf21cae95
Moath Salman <git>
2026-01-31 23:45:34 -0600

    fix: correct module paths for vpc and eks modules

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -1,21 +1,21 @@
 # DO NOT REMOVE DUMMY MODULE references and their code, they should remain as examples
-module "module1" {
-  source = "../../dummy-module-1"
-  # ... any required variables for module1
-  greeting = var.greeting
+# module "module1" {
+#   source = "../../dummy-module-1"
+#   # ... any required variables for module1
+#   greeting = var.greeting
 
-}
+# }
 
-module "module2" {
-  source = "../../dummy-module-2"
+# module "module2" {
+#   source = "../../dummy-module-2"
 
-  input_from_module1 = module.module1.greeting_message
-  # ... any other required variables for module2
-}
+#   input_from_module1 = module.module1.greeting_message
+#   # ... any other required variables for module2
+# }
 
 module "vpc-module" {
 
-  source = "../vpc-module"
+  source = "../../vpc-module"
 
   project_name         = var.project_name
   vpc_cidr             = var.vpc_cidr
@@ -25,7 +25,7 @@ module "vpc-module" {
 
 }
 
-module "eks-module" {
-  source = "../eks-module"
+# module "eks-module" {
+#   source = "../eks-module"
   
-  }
+#   }
