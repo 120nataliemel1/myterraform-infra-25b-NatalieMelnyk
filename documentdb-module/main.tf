957bf7d34d4091bbdbe37fbe0f8e1a202320224f
Vadym1001 <vadym.ryb@edu.312school.com>
2026-01-31 18:03:15 -0600

    feat: add sg for documentDB

---
+++ documentdb-module/main.tf
@@ -0,0 +1,28 @@
+# DocumentDB Module
+#VPC ID, Private Subnet IDs, EKS Node Security Group ID are will be created latter and attached to this module.
+
+#Secutity Group for DocumentDB
+resource "aws_security_group" "documentdb" {
+name        = "${var.name_prefix}-documentdb-sg"
+description = "Security group for DocumentDB cluster"
+vpc_id      = var.vpc_id`
+
+`ingress {
+description     = "Allow MongoDB traffic from EKS nodes"
+from_port       = 27017
+to_port         = 27017
+protocol        = "tcp"
+security_groups = [var.eks_node_sg_id]
+}`
+
+`egress {
+from_port   = 0
+to_port     = 0
+protocol    = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}`
+
+`tags = merge(var.tags, {
+Name = "${var.name_prefix}-documentdb-sg"
+})
+}`
