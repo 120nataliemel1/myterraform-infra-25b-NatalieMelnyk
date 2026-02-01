a76251e1748e3458bf2bc229e8f5c20605ab90c0
Moath Salman <git>
2026-01-31 22:34:17 -0600

    feat(vpc-module): create VPC, subnets, routing, outputs, and environment-driven variables

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -12,3 +12,20 @@ module "module2" {
   input_from_module1 = module.module1.greeting_message
   # ... any other required variables for module2
 }
+
+module "vpc-module" {
+
+  source = "../vpc-module"
+
+  project_name         = var.project_name
+  vpc_cidr             = var.vpc_cidr
+  azs                  = var.azs
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+
+}
+
+module "eks-module" {
+  source = "../eks-module"
+  
+  }
---
+++ vpc-module/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  type        = string
+  description = "Used for naming/tagging resources"
+} 
+
+variable "vpc_cidr" { 
+  type        = string
+  description = "CIDR block for the VPC (ex: 10.0.0.0/16)"
+} 
+
+variable "azs" {
+  type        = list(string)
+  description = "List of AZs to place subnets in (ex: [us-east-1a, us-east-1b, us-east-1c])"
+} 
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDRs for public subnets (3 items)"
+} 
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "List of CIDRs for private subnets (3 items)"
+} 
