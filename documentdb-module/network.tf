76cd0dab38eb64aa4c99388a4c25d7ae66471e98
Vadym1001 <vadym.ryb@edu.312school.com>
2026-02-01 20:45:55 -0600

    feat: create network.tf and moved network related resource(Secutity Group for DocumentDB) into a placeholder

---
+++ documentdb-module/network.tf
@@ -0,0 +1,25 @@
+#Secutity Group for DocumentDB
+resource "aws_security_group" "documentdb" {
+name        = "${var.name_prefix}-documentdb-sg"
+description = "Security group for DocumentDB cluster"
+vpc_id      = var.vpc_id`
+
+`ingress {
+description     = "Allow MongoDB traffic from EKS nodes"
+from_port       = 27017
+to_port         = 27017
+protocol        = "tcp"
+security_groups = [var.eks_node_sg_id]
+}`
+
+`egress {
+from_port   = 0
+to_port     = 0
+protocol    = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}`
+
+`tags = merge(var.tags, {
+Name = "${var.name_prefix}-documentdb-sg"
+})
+}`
