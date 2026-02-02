4b306f4d84cacc3894fe4a143bfe91444d735e51
Vadym1001 <vadym.ryb@edu.312school.com>
2026-02-01 23:36:48 -0600

    feat: add two resources (secret manager and a secret versioning)

--- documentdb-module/main.tf
+++ documentdb-module/main.tf
@@ -5,3 +5,18 @@ resource "random_password" "docdb_password" {
   length  = 16
   special = true
 }
+
+# Secrets Manager secret to store DocumentDB credentials
+resource "aws_secretsmanager_secret" "docdb" {
+  name        = "${var.name_prefix}-${var.environment}-docdb-secret"
+  description = "DocumentDB credentials for ${var.environment} environment"
+  tags        = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "docdb" {
+  secret_id     = aws_secretsmanager_secret.docdb.id
+  secret_string = jsonencode({
+    username = "proshop_admin"
+    password = random_password.docdb_password.result
+  })
+}
