cb3cb165ff816f8fed90498ab84e8b86400d6450
Vadym1001 <vadym.ryb@edu.312school.com>
2026-02-01 21:38:55 -0600

    chore: add networking variables, name_prefix, tags for documentdb module

--- documentdb-module/variables.tf
+++ documentdb-module/variables.tf
@@ -1,3 +1,26 @@
-variable "vpc_id" {}
-variable "private_subnet_ids" {}
-variable "eks_node_sg_id" {}
+
+variable "name_prefix" {
+  description = "Prefix used for naming DocumentDB resources"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where DocumentDB will be deployed"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for DocumentDB subnet group"
+  type        = list(string)
+}
+
+variable "eks_node_sg_id" {
+  description = "Security group ID of EKS worker nodes allowed to access DocumentDB"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags applied to all DocumentDB resources"
+  type        = map(string)
+  default     = {}
+}
