4efa23abdea2199954fcfb3080485c1e290bea1b
Vadym1001 <vadym.ryb@edu.312school.com>
2026-02-01 21:06:12 -0600

    fix: fixed tags and minor errors

--- documentdb-module/network.tf
+++ documentdb-module/network.tf
@@ -7,33 +7,36 @@ locals {
 resource "aws_security_group" "documentdb" {
 name        = "${var.name_prefix}-documentdb-sg"
 description = "Security group for DocumentDB cluster"
-vpc_id      = var.vpc_id`
+vpc_id      = var.vpc_id
 
-`ingress {
+ingress {
 description     = "Allow MongoDB traffic from EKS nodes"
 from_port       = 27017
 to_port         = 27017
 protocol        = "tcp"
 security_groups = [var.eks_node_sg_id]
-}`
+}
 
-`egress {
+egress {
 from_port   = 0
 to_port     = 0
 protocol    = "-1"
 cidr_blocks = ["0.0.0.0/0"]
-}`
+}
 
-`tags = merge(var.tags, {
+tags = merge(var.tags, {
 Name = "${var.name_prefix}-documentdb-sg"
 })
-}`
+}
 
 #DocumentDb Subnet Group
 resource "aws_docdb_subnet_group" "this" {
-  name       = "${var.name}-docdb-subnet-group"
+  name       = "${var.name_prefix}-docdb-subnet-group"
   subnet_ids = var.private_subnet_ids
 
-  tags = var.tags
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-docdb-subnet-group"
+  })
 }
+
 
