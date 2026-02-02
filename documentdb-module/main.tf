9c196502e2db5438eef1c65b9d8948907017c511
Vadym1001 <vadym.ryb@edu.312school.com>
2026-02-01 23:43:13 -0600

    feat: add DocumentDb cluster resource

--- documentdb-module/main.tf
+++ documentdb-module/main.tf
@@ -12,11 +12,25 @@ resource "aws_secretsmanager_secret" "docdb" {
   description = "DocumentDB credentials for ${var.environment} environment"
   tags        = var.tags
 }
-
+# Add a version of the secret with actual credentials
 resource "aws_secretsmanager_secret_version" "docdb" {
   secret_id     = aws_secretsmanager_secret.docdb.id
   secret_string = jsonencode({
     username = "proshop_admin"
     password = random_password.docdb_password.result
   })
+}
+
+# DocumentDB Cluster
+resource "aws_docdb_cluster" "this" {
+  cluster_identifier      = "${var.name_prefix}-${var.environment}-docdb-cluster"
+  engine                  = "docdb"
+  engine_version          = "5.0.0"
+  master_username         = "proshop_admin"
+  master_password         = random_password.docdb_password.result
+  db_subnet_group_name    = aws_docdb_subnet_group.this.name
+  vpc_security_group_ids  = [aws_security_group.documentdb.id]
+  skip_final_snapshot     = true
+  apply_immediately       = true
+  storage_encrypted       = false
 }
