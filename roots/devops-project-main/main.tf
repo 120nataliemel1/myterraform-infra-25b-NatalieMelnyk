2c5c90ea11f7071e9a891101a9d518668def51a0
Bakmurat <bakmurat.kubanaliev@tp-link.com>
2026-02-05 01:21:09 -0800

    create resources

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -14,7 +14,7 @@ module "module2" {
 }
 
 module "vpc-module" {
-  source = "../../vpc-module"
+  source               = "../../vpc-module"
   project_name         = var.project_name
   vpc_cidr             = var.vpc_cidr
   azs                  = var.azs
