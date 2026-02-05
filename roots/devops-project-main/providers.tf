8adc478dfe1382faab271db1a431c8d4fa0494f3
Altynai Nadralieva <altynai.nad@edu.312school.com>
2026-02-05 17:31:46 -0600

    test apply to 312 account

--- roots/devops-project-main/providers.tf
+++ roots/devops-project-main/providers.tf
@@ -5,10 +5,8 @@ provider "aws" {
 terraform {
   backend "s3" {
     # bucket         = "project-x-state-bucket-staging"
-    bucket       = "terraform-remote-state-altynai"
-    key          = "dev/versus/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = true
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraformlock"
   }
 }
-
