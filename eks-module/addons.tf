b6d4c66264428aa0c3bc824af5256ee9d9c3efc1
Moath Salman <git>
2026-02-09 23:27:39 -0600

    comment out open_id in eks/addons

--- eks-module/addons.tf
+++ eks-module/addons.tf
@@ -53,7 +53,7 @@ data "aws_eks_cluster" "this" {
 # Trust anchor for IRSA (OIDC provider derived from the cluster issuer URL)
 data "aws_iam_openid_connect_provider" "this" {
   url            = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
-  client_id_list = ["sts.amazonaws.com"] # standard IRSA audience
+#   client_id_list = ["sts.amazonaws.com"] # standard IRSA audience
 }
 
 # IRSA trust policy: allow ONLY the EBS CSI service account to assume this role
