bf1856091fa87a7d710a8b5e04cddd5c0a899de6
Moath Salman <git>
2026-02-09 23:30:47 -0600

    comment out iam-role modules in main.tf

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -40,42 +40,42 @@ module "eks-module" {
   desired_capacity = var.desired_capacity
 }
 
-module "developer_iam_role" {
-  source         = "../../IAM-role-module"
-  environment    = var.environment
-  principal_type = "AWS"
-  principal      = var.trusted_parent_account_id
-  role_name      = "Developer${var.environment}AccessRole-ubuntu25b"
-  policy_json    = var.DeveloperAccessRolePolicy
-}
+# module "developer_iam_role" {
+#   source         = "../../IAM-role-module"
+#   environment    = var.environment
+#   principal_type = "AWS"
+#   principal      = var.trusted_parent_account_id
+#   role_name      = "Developer${var.environment}AccessRole-ubuntu25b"
+#   policy_json    = var.DeveloperAccessRolePolicy
+# }
 
-module "devops_iam_role" {
-  source         = "../../IAM-role-module"
-  environment    = var.environment
-  principal_type = "AWS"
-  principal      = var.trusted_parent_account_id
-  role_name      = "Devops${var.environment}AccessRole-ubuntu25b"
-  policy_json    = var.DevopAccessRolePolicy
-}
+# module "devops_iam_role" {
+#   source         = "../../IAM-role-module"
+#   environment    = var.environment
+#   principal_type = "AWS"
+#   principal      = var.trusted_parent_account_id
+#   role_name      = "Devops${var.environment}AccessRole-ubuntu25b"
+#   policy_json    = var.DevopAccessRolePolicy
+# }
 
 #FOR TEST PURPOSES ONLY NEXT IAM ROLE BLOCKS FOR PRODUCTION NEED TO BE REMOVED WHEN WE HAVE PRODUCTION ACCOUNT
 
-module "developer_prod_role" {
-  source         = "../../IAM-role-module"
-  environment    = "Prod"
-  principal_type = "AWS"
-  principal      = var.trusted_parent_account_id
-  role_name      = "DeveloperProdAccessRole-ubuntu25b"
-  policy_json    = "DeveloperProdAccessRole.json"
-}
+# module "developer_prod_role" {
+#   source         = "../../IAM-role-module"
+#   environment    = "Prod"
+#   principal_type = "AWS"
+#   principal      = var.trusted_parent_account_id
+#   role_name      = "DeveloperProdAccessRole-ubuntu25b"
+#   policy_json    = "DeveloperProdAccessRole.json"
+# }
 
-module "devops_prod_role" {
-  source         = "../../IAM-role-module"
-  environment    = "Prod"
-  principal_type = "AWS"
-  principal      = var.trusted_parent_account_id
-  role_name      = "DevopsProdAccessRole-ubuntu25b"
-  policy_json    = "DevopsProdAccessRole.json"
-}
+# module "devops_prod_role" {
+#   source         = "../../IAM-role-module"
+#   environment    = "Prod"
+#   principal_type = "AWS"
+#   principal      = var.trusted_parent_account_id
+#   role_name      = "DevopsProdAccessRole-ubuntu25b"
+#   policy_json    = "DevopsProdAccessRole.json"
+# }
 
 #--------------------------------------
