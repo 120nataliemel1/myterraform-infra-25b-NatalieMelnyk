af9f39aa2a740cf1d9fcc9760f0ecc4f2ed93642
Altynai Nadralieva <altynai.nad@edu.312school.com>
2026-02-12 19:43:23 -0600

    tf fmt

--- roots/devops-project-main/main.tf
+++ roots/devops-project-main/main.tf
@@ -19,18 +19,18 @@ module "module2" {
 module "rds_mysql" {
   source = "../../rds-mysql-module"
 
-  identifier                 = var.identifier
-  allocated_storage          = var.allocated_storage
-  engine                     = var.engine
-  engine_version             = var.engine_version
-  instance_class             = var.instance_class
-  db_name                    = var.db_name
-  username                   = var.username
-  db_password                = var.db_password
-  parameter_group_name       = var.parameter_group_name
-  publicly_accessible        = var.publicly_accessible
-  db_subnet_group_name       = var.db_subnet_group_name
-  db_subnet_ids              = var.db_subnet_ids
+  identifier           = var.identifier
+  allocated_storage    = var.allocated_storage
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  db_name              = var.db_name
+  username             = var.username
+  db_password          = var.db_password
+  parameter_group_name = var.parameter_group_name
+  publicly_accessible  = var.publicly_accessible
+  db_subnet_group_name = var.db_subnet_group_name
+  db_subnet_ids        = var.db_subnet_ids
   # vpc_security_group_ids     = module.rds_mysql.rds_security_group_id
   db_security_group_name     = var.db_security_group_name
   vpc_id                     = var.vpc_id
